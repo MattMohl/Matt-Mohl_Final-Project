4172fe56f10ebd2f18ea67f2b28e0d45623b53ab
Matt Mohl <MattMohl@fullsail.edu>
2014-02-04 12:36:13 -0500

    Added user controller for login / register / logout

--- themanapool.com/js/controllers/user.js
+++ themanapool.com/js/controllers/user.js
@@ -1,5 +1,63 @@
-themanapool.controller('user', function($rootScope, $scope){
+themanapool.controller('user', ['$rootScope', '$scope', '$firebase', '$location', 'UserService',
+	function($rootScope, $scope, $firebase, $location, userService){
 
-	console.log('user');
+	console.log('CONTROLLER::user');
 
-});
+	// View-Models
+	/* 
+	$scope.regForm
+	$scope.logForm
+
+	$scope.username
+	$scope.email1
+	$scope.p1
+
+	$scope.email2
+	$scope.p2
+	*/
+	
+	// userService.instantiateUser('newguy');
+
+	$scope.register = function() {
+		/*
+			Must create an object inside firebase for the 
+			new username with:
+			level -> 1
+			exp -> 0
+		*/
+		console.log('register');
+
+		// Create the Firebase user
+		$rootScope.auth.createUser($scope.email1, $scope.p1,
+			function(error, user) {
+				if(!error) {
+					console.log('User Id: ' + user.id + ', Email: ' + user.email);
+
+					// Set current user, instantiate, and login
+					$scope.currentUser = user;
+					$scope.currentUser.username = $scope.username;
+					userService.instantiateUser($scope.currentUser.id, $scope.username);
+					
+					$rootScope.auth.login('password', {
+						email: $scope.email1,
+						password: $scope.p1
+					});
+					$location.path('/mydecks');
+				}
+			});
+	}
+
+	$scope.login = function() {
+		$rootScope.auth.login('password', {
+			email: $scope.email2,
+			password: $scope.p2
+		});
+		$location.path('/browse');
+	}
+
+	$scope.logout = function() {
+		console.log('out');
+		$rootScope.auth.logout();
+		$location.path('/');
+	}
+}]);
