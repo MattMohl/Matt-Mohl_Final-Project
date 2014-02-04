53465dedbc4fae33e7b0509eb9d2b92f8d29d13c
Matt Mohl <MattMohl@fullsail.edu>
2014-02-04 12:35:24 -0500

    Added config file to control site navigation and user authentication

--- themanapool.com/js/controllers/config.js
+++ themanapool.com/js/controllers/config.js
@@ -1,16 +1,53 @@
-var themanapool = angular.module('themanapool', ['ngRoute', '$firebase']);
+var themanapool = angular.module('themanapool', ['ngRoute', 'firebase']);
 
 themanapool.config(function($routeProvider) {
 	$routeProvider
 		.when('/', {
-			templateUrl: 'views/home.html',
-			controller: 'user'
+			controller: 'user',
+			templateUrl: 'views/home.html'
+		})
+		.when('/mydecks', {
+			controller: 'user',
+			templateUrl: 'views/user-decks.html'
+		})
+		.when('/browse', {
+			controller: 'browser',
+			templateUrl: 'views/browse.html'
+		})
+		.when('/browse/sets', {
+			controller: 'browser',
+			templateUrl: 'views/sets.html'
+		})
+		.when('/browse/sets/:set', {
+			controller: 'browser',
+			templateUrl: 'views/set-cards.html'
+		})
+		.when('/browse/advanced', {
+			controller: 'browser',
+			templateUrl: 'views/advanced.html'
 		})
 		.otherwise({redirectTo: '/'});
 });
 
-themanapool.run(['$rootScope', '$firebaseAuth', '$firebase', '$location', function($rootScope, $firebaseAuth, $firebase, $location) {
+themanapool.run(['$rootScope', '$firebase', '$firebaseSimpleLogin', '$location', function($rootScope, $firebase, $firebaseSimpleLogin, $location) {
+	console.log('RUN');
 
-	console.log('run');
+	var manapool = new Firebase('https://manapool.firebaseio.com');
+
+	// Instantiate an auth reference
+	$rootScope.auth = new FirebaseSimpleLogin(manapool, 
+		function(error, user) {
+			console.log(error, user);
+			if(angular.isObject(user)) {
+				console.log('is user '+user.id);
+				if($location.path() == '/') {
+					console.log('redir');
+					$location.path('/browse');
+				}
+			}else {
+				console.log('not user '+user);
+				$location.path('/');
+			}
+	});
 
 }]);
