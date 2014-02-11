29f083fcbd36aaa5c1878a8b83fa0d1f7cf9c739
Matt Mohl <MattMohl@fullsail.edu>
2014-02-11 14:06:07 -0500

    Reworked the browser controller to instantiate only used information

--- themanapool.com/js/controllers/browser.js
+++ themanapool.com/js/controllers/browser.js
@@ -3,14 +3,10 @@ themanapool.controller('browser', ['$route', '$rootScope', '$scope', '$firebase'
 
 		console.log('CONTROLLER::browser');
 
-		// Get 5 sets for Browse
-		if($location.path() === '/browse') {
-			$scope.setList = browseService.getSets(5);
-		}
 		// Get all sets
-		if($location.path() === '/browse/sets') {
-			$scope.allSets = browseService.getSets(200);
-		}
+		$scope.fewSets = browseService.getSets(5);
+		$scope.allSets = browseService.getSets(300);
+
 		// Browse Set Cards
 		$scope.currentSet = browseService.getSet($route.current.params.set);
 		$scope.setCards = browseService.getSetCards($route.current.params.set, 20);
@@ -19,36 +15,6 @@ themanapool.controller('browser', ['$route', '$rootScope', '$scope', '$firebase'
 		// Advanced inits
 		if($location.path() === '/browse/advanced') {
 			$rootScope.searchParams = [];
-			$rootScope.allSetsResults = browseService.getSetsLarge(200);
+			$rootScope.allSetsResults = browseService.getSetsLarge(2000);
 		}
-
-		$scope.addParam = function(type, value) {
-			$rootScope.searchParams.push({'type': type, 'value': value});
-			$scope.name 		= '';
-			$scope.rules 		= '';
-			$scope.set 			= '';
-			$scope.block 		= '';
-			$scope.color 		= '';
-			$scope.type 		= '';
-			$scope.subtype 		= '';
-			$scope.cmc 			= '';
-			$scope.mana 		= '';
-			$scope.power 		= '';
-			$scope.toughness 	= '';
-			$scope.flavor 		= '';
-			$scope.artist 		= '';
-			$scope.rarity 		= '';
-		}
-
-		// showResults = function(set, btn) {
-			
-
-		// 		<ul id="{{set.code}}-list">
-
-		// 		</ul>
-			
-		// 	console.log(btn.style);
-
-		// 	
-		// }
 }]);
